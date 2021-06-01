4c5b16578a8deee3701a739334f7f39ca96e7f0c
turk-webdev <uerdin@mail.sfsu.edu>
2021-06-01 11:36:06 -0700

    Initial commit

--- bin/OmniWheels.c
+++ bin/OmniWheels.c
@@ -23,109 +23,109 @@ void get_pwm() {
 }
 
 void run_omni_demo() {
-    printf("Demo init motor...\n");
+    DEBUG("Demo init motor...\n");
     init_omni(); 
     long run_start = micros(); 
 
     // 1) Drive forward for 2sec
-    printf("PRANCE FORWARD...\n");
+    DEBUG("PRANCE FORWARD...\n");
     
     motor_drive(FORWARD, pwm);
     interval(3e6);
-    printf("STOP!\n");
+    DEBUG("STOP!\n");
     stop_motors();
     run_start = micros();
     interval(1.75e6);
 
     // 2) Drive backward for 2sec
-    printf("BOOGIE DOWN...\n");
+    DEBUG("BOOGIE DOWN...\n");
     run_start = micros();
     motor_drive(BACKWARD, pwm);
     interval(3e6);
-    printf("STOP!\n");
+    DEBUG("STOP!\n");
     stop_motors();
     run_start = micros();
     interval(1.75e6);
 
     // 3) Rotate left for 2sec
-    printf("Rotate CCW\n");
+    DEBUG("Rotate CCW\n");
     run_start = micros();
     rotate_ccw(pwm);
     interval(3e6);
-    printf("STOP!\n");
+    DEBUG("STOP!\n");
     stop_motors();
     run_start = micros();
     interval(1.75e6);
     
     // 4) Rotate right for 2sec
-    printf("Rotate CW\n");
+    DEBUG("Rotate CW\n");
     run_start = micros();
     rotate_cw(pwm);
     interval(3e6);
-    printf("STOP!\n");
+    DEBUG("STOP!\n");
     stop_motors();
     run_start = micros();
     interval(1.75e6);
 
     // 5) Strafe left for 2sec
-    printf("SASHAY LEFT...\n");
+    DEBUG("SASHAY LEFT...\n");
     run_start = micros();
     strafe_left(pwm);
     interval(3e6);
-    printf("STOP!\n");
+    DEBUG("STOP!\n");
     stop_motors();
     run_start = micros();
     interval(1.75e6);
 
     // 6) Strafe right for 2sec
-    printf("SHIMMY RIGHT...\n");
+    DEBUG("SHIMMY RIGHT...\n");
     run_start = micros();
     strafe_right(pwm);
     interval(3e6);
-    printf("STOP!\n");
+    DEBUG("STOP!\n");
     stop_motors();
     run_start = micros();
     interval(1.75e6);
 
     // 7) Left diagonal
-    printf("Left Diag Fwd\n");
+    DEBUG("Left Diag Fwd\n");
     run_start = micros();
     diag_left(FORWARD, pwm);
     interval(3e6);
-    printf("STOP!\n");
+    DEBUG("STOP!\n");
     stop_motors();
     run_start = micros();
     interval(1.75e6);
-    printf("Left Diag Bwd\n");
+    DEBUG("Left Diag Bwd\n");
     run_start = micros();
     diag_left(BACKWARD, pwm);
     interval(3e6);
-    printf("STOP!\n");
+    DEBUG("STOP!\n");
     stop_motors();
     run_start = micros();
     interval(1.75e6);
 
     // 8) Right diagonal
-    printf("Right Diag Fwd\n");
+    DEBUG("Right Diag Fwd\n");
     run_start = micros();
     diag_right(FORWARD, pwm);
     interval(3e6);
-    printf("STOP!\n");
+    DEBUG("STOP!\n");
     stop_motors();
     run_start = micros();
     interval(1.75e6);
-    printf("Right Diag Bwd\n");
+    DEBUG("Right Diag Bwd\n");
     run_start = micros();
     diag_right(BACKWARD, pwm);
     interval(3e6);
-    printf("STOP!\n");
+    DEBUG("STOP!\n");
     stop_motors();
     run_start = micros();
     interval(1.75e6);
 }
 
 void init_omni() {
-    printf("OmniWheels init_omni()\n");
+    DEBUG("OmniWheels init_omni()\n");
     Motor_Init();
 }
 
